70432d36680d2996f76835ac3c2211e4c234236b
Ken <banyarnaingcodes@gmail.com>
2022-03-03 07:58:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+pwd = input("What is the master password?")
+def view():
+  pass
+def add():
+  name = input('Account Name: ')
+  pwd = input('Password: ')
+
+  with open('password.txt', 'a') as f: 
+    f.write(name + "|" + pwd + "\n")
+while True:
+  mode = input("Would you like to add a new password or view existing ones (view, add)?Press `q` to quit.").lower()
+  if mode == "q":
+      break
+  if mode == "view":
+    view()
+  elif mode == "add":
+    add()
+  else:
+    print("Invalid mode.")
+    continue
+    
